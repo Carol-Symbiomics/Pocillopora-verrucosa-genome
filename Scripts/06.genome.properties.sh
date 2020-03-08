758f1fa76661c2ad37e1437cd5e73ef7d9db126a
Carol-Symbiomics <carol.buitragolopez@kaust.edu.sa>
2020-03-08 14:51:16 +0300

    Update Kmer count R script

--- Scripts/06.genome.properties.sh
+++ Scripts/06.genome.properties.sh
@@ -12,4 +12,4 @@ jellyfish count -t 20 -C -m 31 -s 5G -o Pver_31mer *.fq
 
 jellyfish histo -h 3000000 -t 20 -o Pver_31mer.histo Pver_31mer
 
-# Plots and kmer count was done using an the R script 06.Pver.jellyfish.khmer.spectra.R
+# Plots and kmer count was done using an the R script Pver.jellyfish.khmer.spectra.R in the Supplementary_scripts folder
--- Scripts/Supplementary_scripts/Pver.jellyfish.khmer.spectra.R
+++ Scripts/Supplementary_scripts/Pver.jellyfish.khmer.spectra.R
@@ -1,6 +1,6 @@
-# Genome size estimation using Jellyfish 
+# Genome size estimation using Jellyfish
 
-setwd("~/Genomes/corals/Pocillopora.verrucosa/01_Pver_lib4_Genome_properties")
+setwd("~/Genomes/corals/Pocillopora.verrucosa/05_Pver_lib4_genome_properties")
 
 Pver_31mer <- read.table("Pver_31mer.histo") #25622 lanes
 # het_peak 76 4777149
@@ -35,11 +35,17 @@ sum(as.numeric(Pver_25mer[117:29660,1]*Pver_25mer[117:29660,2]))/159
 134341370 + 272892790
 # 407,234,160  Diploid genome size size 25mer
 
-pdf("Khmer_spectra_jellyfish.pdf") 
+marks <- c(0,1000000,2000000,3000000,4000000,5000000)
+marks_label <- c(0,1,2,3,4,5)
+
+png("Khmer_spectra_jellyfish.png", width = 600, height = 300) 
 # Create a plot
-par(mar = c(6.5, 6.5, 3, 0.5), mgp = c(4.5, 1, 0))
-plot(Pver_31mer[4:300,], type="l", col="gray18", las=1, lwd = 2, lty=4, xlab="Kmer-Coverage", ylab="Kmer counts frequency", main="K-hmer spectra")
+par(mar = c(5, 7, 2, 2))
+plot(Pver_31mer[4:300,], type="l", col="gray18", lwd = 2, lty=4, axes = F, frame.plot=F, ann=F)
+axis(1, pos=0, las=1)
+title(xlab = "Kmer-Coverage", cex.lab = 1, line = 2)
+axis(2, pos=0, las=1, at=marks, labels = marks_label)
+title(ylab = "Kmer counts frequency (millions)", cex.lab = 1, line = 1.5)
 lines(Pver_25mer[4:300,], type="l", col="gray18", las=1, lwd = 2)
 legend("topright", c("k = 25", "k = 31"), cex=1, col = "gray18", lty=c(1,4) , bty = "n")
 dev.off() 
-
